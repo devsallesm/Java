f9e3453f76bf04bc313bb848d6f9a169e928107c
svlles <160500308+svlles@users.noreply.github.com>
2025-03-24 22:43:13 -0300

    Add files via upload

---
+++ aulasMagno/Aula3.java
@@ -0,0 +1,139 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula;
+
+/**
+ *
+ * @author Aluno
+ */
+
+import java.util.Scanner;
+
+public class Aula3 {
+
+    public static void main(String[] args) {
+        /*
+        int numero = 13;
+        System.out.println("Ola mundo");
+                
+
+         Scanner teclado = new Scanner(System.in);
+        System.out.println(numero);
+        System.out.println("digite um numero: ");
+        numero = teclado.nextInt();
+        
+        System.out.println(numero);
+
+         if (numero > 0) {
+            System.out.println("o numero é positivo");
+        }
+        
+        if (numero < 0) {
+            System.out.println("o numero é negativo");
+        }
+        
+        if (numero == 0) {
+            System.out.println("o numero é zero");
+        } 
+
+          fim 0exercicio 
+       
+         if (numero %2==0) {
+            System.out.println("o numero é par");
+        }
+        
+        if ( numero %2== 1 ) {
+            System.out.println("o numero é impar");
+        } 
+        int idade = 17;
+
+        if ( idade >= 18){
+            System.out.println("VocÊ é maior de idade ");
+        }       
+        else {
+            System.out.println("VocÊ é menor de idade ");
+        }
+       
+        Scanner Tec = new Scanner(System.in);
+        int idade = 0;
+        idade = Tec.nextInt();
+        if ( idade <12 ) {
+            System.out.println("O usuario é uma criança ");
+        }
+        else if ( idade >12 || idade < 18 ) {
+            System.out.println("O usuario é um adolescente ");
+        }
+        else if ( idade >18 || idade < 65 ) {
+            System.out.println("O usuario é um adulto ");
+        }
+        else {
+            System.out.println("O usuario é um idoso ");
+        }
+        */
+        
+        /*
+        int num = 0 ; 
+         Scanner Tec = new Scanner(System.in);
+        num = Tec.nextInt();
+        if ( num > 0 ) {
+            System.out.println("Positivo");
+        }
+        else if (num  < 0 ) {
+            System.out.println("Negativo");
+        }
+        else {
+            System.out.println("Zero");
+        }
+        */
+         
+        
+        /*
+        Scanner Tec = new Scanner(System.in);
+        System.out.println("Digite um numero inteiro");
+        int numero1 = Tec.nextInt();
+        System.out.println("Digite outro numero inteiro");
+        int numero2 = Tec.nextInt();
+        System.out.println("Digite 1 para soma, 2 para subtração, 3 para multiplicação e 4 para divisão");
+        int operacao = Tec.nextInt();
+        int resultado = 0;
+        
+        if (operacao == 1 ) {           
+        resultado = numero1 + numero2;
+        System.out.println("o resultado da soma de "+numero1+" + "+numero2+" eh "+resultado);
+        }
+        else if (operacao == 2 ) {           
+        resultado = numero1 - numero2;
+        System.out.println("o resultado da subtracao de "+numero1+" - "+numero2+" eh "+resultado);
+        }
+        else if (operacao == 3 ) {
+        resultado = numero1 * numero2;
+        System.out.println("o resultado da multiplicacao de "+numero1+" x "+numero2+" eh "+resultado);
+        }
+        else if (operacao == 4 ) {
+        resultado = numero1 / numero2;
+        System.out.println("o resultado da divisao de "+numero1+" / "+numero2+" eh "+resultado);
+        }
+        else {
+            System.out.println("Operacao nao reconhecida");
+        }
+        */
+        
+        
+        /*
+               Scanner Tec = new Scanner(System.in);
+        System.out.println("Digite um numero inteiro");
+        int numero1 = Tec.nextInt();
+        
+              if (numero1 %3 == 0 ) {           
+        System.out.println("o numero "+numero1+" e um multiplo de 3");
+        }
+              else {
+                  System.out.println("o numero "+numero1+" nao ée um multiplo de 3");
+              }
+        */
+        
+}
+}
+
